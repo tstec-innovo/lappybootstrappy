90a532fe94f84b61ac440cbea2f33bbd185f08eb
neutron37 <tom@satori.us>
2018-10-06 15:57:48 +0200

    Additional commands to be ported to ansible.

--- additional.sh
+++ additional.sh
@@ -5,3 +5,8 @@ brew cask install alfred
 brew cask install shiftit
 brew install zsh
 sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
+brew cask install 1password
+open ~/Documents/licenses/my.onepassword7-license-mac
+atom ~/Documents/licenses/alfred.license
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+plugins=(git colored-man colorize pip python brew osx zsh-syntax-highlighting)
