e003a53636004f1c8fb1c750c3140831a54a8822
neutron37 <tom@satori.us>
2019-07-02 10:48:03 +0200

    Additional commands to be ported to ansible.

--- additional.sh
+++ additional.sh
@@ -10,3 +10,12 @@ open ~/Documents/licenses/my.onepassword7-license-mac
 atom ~/Documents/licenses/alfred.license
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 plugins=(git colored-man colorize pip python brew osx zsh-syntax-highlighting)
+brew cask install docker
+brew cask install google-chrome
+brew cask install vagrant
+brew install wget
+brew install php@7.1 composer
+brew cask install balenaetcher
+brew cask install vscodium
+brew cask install signal
+npm install -g localtunnel
