124a8c7b4d50b4679686e4442ce58d40db72e47d
tom <tom@satori.us>
2019-07-03 20:45:39 +0200

    Reorder additional.sh

--- additional.sh
+++ additional.sh
@@ -3,13 +3,9 @@ brew cask install atom
 defaults write com.apple.dock static-only -bool TRUE; killall Dock
 brew cask install alfred
 brew cask install shiftit
-brew install zsh
-sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
 brew cask install 1password
-open ~/Documents/licenses/my.onepassword7-license-mac
+atom ~/Documents/licenses/my.onepassword7-license-mac
 atom ~/Documents/licenses/alfred.license
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-plugins=(git colored-man colorize pip python brew osx zsh-syntax-highlighting)
 brew cask install docker
 brew cask install google-chrome
 brew cask install vagrant
@@ -19,3 +15,5 @@ brew cask install balenaetcher
 brew cask install vscodium
 brew cask install signal
 npm install -g localtunnel
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+plugins=(git colored-man colorize pip python brew osx zsh-syntax-highlighting)
