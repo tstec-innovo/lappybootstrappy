33199c46bc107abf0ebbb6450912184abe106f42
tom <tom@satori.us>
2019-07-04 00:55:48 +0200

    Make pretty

--- makefile
+++ makefile
@@ -9,15 +9,15 @@ ssh_keygen:
 
 # Set shell defaults to support Susudoio
 set_shell_defaults:
-ifneq (,$(wildcard $(HOME)/.config/set_shell_defaults.ok))
-	@echo "======== Ensuring dotfile defaults. ==================================="
+	@echo "======== Ensuring Dotfile Defaults ===================================="
+ifeq (,$(wildcard $(HOME)/.config/set_shell_defaults.ok))
 	$(BASHLIB_SRC_DIR)/set_shell_defaults.sh
 endif
 
 # Prerequisites
 prerequisites:
 ifeq ($(UNAME_S),Darwin)
-	@echo "======== Ensuring Darwin prerequisites. ==============================="
+	@echo "======== Ensuring Darwin Prerequisites ================================"
 ifeq ($(shell which brew),)
 	@echo "* Installing homebrew."
 	$(BASHLIB_SRC_DIR)/macos_install_brew.sh
--- src/ssh_keygen.sh
+++ src/ssh_keygen.sh
@@ -26,5 +26,7 @@ bashlib::print_cmd   '[ -d $HOME/.ssh ] || mkdir -p "${HOME}/.ssh"'
 bashlib::print_cmd   '[ -f $HOME/.ssh/id_rsa ] || ssh-keygen -b4096 -f"${HOME}/.ssh/id_rsa"'
                       [ -f $HOME/.ssh/id_rsa ] || ssh-keygen -b4096 -f"${HOME}/.ssh/id_rsa"
 bashlib::print_cmd   'cat "${HOME}/.ssh/id_rsa.pub"'
+bashlib::msg_stdout   "$STYLE_MAGENTA********* Add this key to your git account! ******************$STYLE_GREEN $STYLE_BOLD"
                       cat "${HOME}/.ssh/id_rsa.pub"
-bashlib::msg_stdout  "------------------------------------------------------------"
+bashlib::msg_stdout   "$STYLE_NORMAL$STYLE_MAGENTA**************************************************************"
+bashlib::msg_stdout  "$STYLE_NORMAL------------------------------------------------------------"
